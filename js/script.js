98a396ee16eed094324b47bb78802ecf1bd13dcd
unknown <flowerrosa76@gmail.com>
2024-04-07 14:57:10 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let navigation=document.querySelector('.navigation');
+document.querySelector('.toggle').onclick =function(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const tabItem= document.querySelectorAll('.tabs_btn_item');
+const tabContent =document.querySelectorAll('.tabs_content_item');
+
+
+tabItem.forEach(element =>{
+    element.addEventListener('click',openItem);
+});
+function openItem(e){
+    const tabTarget =e.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(item =>{
+        item.classList.remove('tabs_btn_item-active');
+    });
+
+    tabContent.forEach(item =>{
+        item.classList.remove('tabs_content_item-active');
+    });
+
+    tabTarget.classList.add('tabs_btn_item-active');
+    document.querySelector(`#${button}`).classList.add('tabs_content_item-active');
+}
